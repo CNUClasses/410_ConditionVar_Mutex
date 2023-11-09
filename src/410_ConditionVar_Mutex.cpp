f25a13dfd329527ea963889b12e0eab1fb57e3be
Keith Perkins <keith.perkins@cnu.edu>
2023-11-08 23:24:58 -0500

    simplified

--- src/410_ConditionVar_Mutex.cpp
+++ src/410_ConditionVar_Mutex.cpp
@@ -19,27 +19,18 @@ mutex 				m;
 condition_variable 	cv;
 bool 				ready = false;	//the condition
 
-//if following defined then there is a race condition
-//where waiter can miss being notified, 
+//there is a race condition
+//where waiter can miss being notified,
 //or a spurious wakeup will cause incorrect behavior
-#define INCORRECT_NO_SHARED_VAR
-
 void wait() {
 	std::thread::id this_id = std::this_thread::get_id();
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));	//wait before aquiring lock below
 	
 	unique_lock<mutex> lck(m);
 	cout <<"Thread "<<this_id<< ": About to wait"<< endl; //makes it hard to see thread switches
-#ifdef INCORRECT_NO_SHARED_VAR
 
-
+	//uh ohh no check for condition
 	cv.wait(lck);
-#else
-	while (!ready){
-		cout <<"Thread "<<this_id<< ": about to call cv.wait()"<< endl;
-		cv.wait(lck);
-	}
-#endif
 
 	cout <<"Thread "<<this_id<< ": Finished waiting, exiting thread"<< endl; //makes it hard to see thread switches
 }
@@ -47,24 +38,43 @@ void wait() {
 void notify() {
 
 	std::thread::id this_id = std::this_thread::get_id();
-#ifdef INCORRECT_NO_SHARED_VAR
 	{
 		unique_lock<mutex> lck(m);
 		cout <<"Thread "<<this_id<< ": About to notify"<< endl; //makes it hard to see thread switches
 	}	
-#else
-	{
-		unique_lock<mutex> lck(m);
-		cout <<"Thread "<<this_id<< ": About to notify"<< endl; //makes it hard to see thread switches
-		ready = true;
-	}
-#endif	
+
 	cv.notify_all();
 	
 	cout <<"Thread "<<this_id<< ": Notified, exiting thread"<< endl; //makes it hard to see thread switches
 }
 
-
+////correct solution
+//void wait() {
+//	std::thread::id this_id = std::this_thread::get_id();
+//	std::this_thread::sleep_for(std::chrono::milliseconds(1000));	//wait before aquiring lock below
+//
+//	unique_lock<mutex> lck(m);
+//	cout <<"Thread "<<this_id<< ": About to wait"<< endl; //makes it hard to see thread switches
+//
+//	while (!ready){
+//		cout <<"Thread "<<this_id<< ": about to call cv.wait()"<< endl;
+//		cv.wait(lck);
+//	}
+//
+//	cout <<"Thread "<<this_id<< ": Finished waiting, exiting thread"<< endl; //makes it hard to see thread switches
+//}
+//
+//void notify() {
+//	std::thread::id this_id = std::this_thread::get_id();
+//	{
+//		unique_lock<mutex> lck(m);
+//		cout <<"Thread "<<this_id<< ": About to notify"<< endl; //makes it hard to see thread switches
+//		ready = true;
+//	}
+//	cv.notify_all();
+//
+//	cout <<"Thread "<<this_id<< ": Notified, exiting thread"<< endl; //makes it hard to see thread switches
+//}
 int main(){
 	thread t_w(wait);
 	thread t_n(notify);	
